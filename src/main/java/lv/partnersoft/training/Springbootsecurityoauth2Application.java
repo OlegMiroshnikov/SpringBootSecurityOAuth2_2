e8e428b03d81306b8cee4723e0b688c1de783e41
Oleg Miroshnikov <oleg.psoft@gmail.com>
2020-11-19 12:32:47 +0200

    Initial commit. Access user information for login success page.

---
+++ src/main/java/lv/partnersoft/training/Springbootsecurityoauth2Application.java
@@ -0,0 +1,13 @@
+package lv.partnersoft.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootsecurityoauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springbootsecurityoauth2Application.class, args);
+    }
+
+}
