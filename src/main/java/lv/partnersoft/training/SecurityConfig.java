7751b031a0d826894f67f0b55523b0bd10c5115c
Oleg Miroshnikov <oleg.psoft@gmail.com>
2020-11-25 17:40:02 +0200

    Custom token enpoint.

--- src/main/java/lv/partnersoft/training/SecurityConfig.java
+++ src/main/java/lv/partnersoft/training/SecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.endpoint.NimbusAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
 import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
 import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizationRequestRepository;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
@@ -17,6 +20,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new HttpSessionOAuth2AuthorizationRequestRepository();
     }
 
+    @Bean
+    public OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest>
+    accessTokenResponseClient() {
+
+        return new NimbusAuthorizationCodeTokenResponseClient();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -28,8 +38,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/oauth_login")
                 .authorizationEndpoint()
                 .baseUri("/oauth2/authorize-client")
-                .authorizationRequestRepository(authorizationRequestRepository());
-//                .defaultSuccessUrl("/loginSuccess", true)
-//                .failureUrl("/loginFailure");
+                .authorizationRequestRepository(authorizationRequestRepository())
+                .and()
+                .tokenEndpoint()
+                .accessTokenResponseClient(accessTokenResponseClient())
+                .and()
+                .defaultSuccessUrl("/loginSuccess", true)
+                .failureUrl("/loginFailure");
     }
 }
